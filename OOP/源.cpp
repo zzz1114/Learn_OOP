f166e0665ee9902c59b382d85c554a5c1523ba9c
zzz1114 <2759579990@qq.com>
2021-08-26 14:14:33 +0800

    将new出来的内存销毁，使用delete,如果是数组，则用delete[]表示销毁指针指向的那一块内存

--- OOP/源.cpp
+++ OOP/源.cpp
@@ -2,26 +2,32 @@
 
 //new申请内存
 //申请一个10*5的二维数组，并返回首地址
-void get2DArr(int(*p)[5])
+void get2DArr(int(*&p)[5])
 {
-	p = new int[10][5];	//int (*)[5] 类型和 int** 类型是不一样的。一个数指向数组的指针，一个是指向指针的指针
+	p = new int[2][5];	//int (*)[5] 类型和 int** 类型是不一样的。一个数指向数组的指针，一个是指向指针的指针
 }
 
-void delet2DArr(int(*p)[5])
+void delete2DArr(int(*&p)[5])
 {
-	free(p);
+	//delete p;		//释放数组的时候需要加一个中括号才可以
+	delete[] p;	
+	p = NULL;
 }
 
 int main()
 {
-	/*
 	int(*p)[5] = NULL;
 	get2DArr(p);
+	//p = new int[10][5];
 	p[0][1] = 12;
 	p[0][4] = 12;
 	p[1][1] = 23;
 	p[1][4] = 34;	
-	*/
-	std::cout << 0 % 5 << std::endl;
+
+	delete2DArr(p);
+	std::cout << (int)p << std::endl;
+	std::cout << "p " << (p == NULL) << std::endl;
+	//std::cout << p[0][1] << " " << p[0][4] << std::endl;
+	//std::cout << p[1][1] << " " << p[1][4] << std::endl;
 	return 0;
 }
