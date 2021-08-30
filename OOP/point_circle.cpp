b8334b52b1c4a5dd9733709254c745fd8be49223
zzz1114 <2759579990@qq.com>
2021-08-30 23:44:22 +0800

    this指针、成员变量和成员函数跟开存储、常对象、常函数、友元

--- OOP/point_circle.cpp
+++ OOP/point_circle.cpp
@@ -1,160 +1,194 @@
-#include <iostream>
-
-const double EPSILON = 1e-5;
-
-class Point
-{
-public:
-	//无参构造函数
-	Point() : X(0), Y(0) {}
-	//有参构造函数
-	Point(int x, int y) : X(x), Y(y) { std::cout << "Point构造函数调用" << std::endl; }
-	Point(int x) :X(x), Y(0) {}
-	//拷贝构造函数
-	Point(Point& p) { X = p.getCoor_X(); Y = p.getCoor_Y(); std::cout << "Point拷贝构造调用" << std::endl; }
-	Point(const Point& p) { X = p.X; Y = p.Y; std::cout << "Point(const)拷贝构造调用" << std::endl;
-	}
-	~Point() { std::cout << "Point析构函数调用" << std::endl; }
-	//设置坐标
-	void setCoor(int x, int y)
-	{
-		X = x;
-		Y = y;
-	}
-	//获取位置
-	int getCoor_X() { return X; }
-	int getCoor_Y() { return Y; }
-	//计算与另一个点之间的距离
-	double GetDistance(Point p)
-	{
-		int xDis = p.getCoor_X() - getCoor_X();
-		int yDis = p.getCoor_Y() - getCoor_Y();
-		return sqrt(xDis * xDis + yDis * yDis);
-	}
-
-	//静态成员变量
-	static int value;
-	static void fun()
-	{
-		int a = value;
-		int b = m;
-		std::cout << "静态成员函数" << std::endl;
-	}
-	void fun3() { fun2(); }
-
-private:
-	int X;
-	int Y;
-	static int m;
-	static void fun2();
-};
-
-int Point::value = 0;
-int Point::m = 10;
-
-void Point::fun2()
-{
-	std::cout << "静态成员函数" << std::endl;
-}
-
-
-class Circle {
-public:
-	Circle(int r, int x, int y) :Radius(r), Center(x, y) { std::cout << "Circle构造函数调用" << std::endl; }
-	Circle(Circle& c) { Radius = c.getRadius(); Center = c.	getCenter(); std::cout << "Circle拷贝构造函数调用" << std::endl; }
-	~Circle() { std::cout << "Circle析构函数调用" << std::endl; }
-	void setCenter(int x, int y) { Center.setCoor(x, y); }
-	void setRadius(int r) { Radius = r; }
-	int getRadius() { return Radius; }
-	Point getCenter() { return Center; }
-private:
-	//半径
-	int Radius;
-	//圆心坐标
-	Point Center;
-};
-
-//判断点是否在圆上
-//返回值  大于0 在圆外；等于0 在圆上；小于0 在圆内
-int isPointOnCircle(Circle c, Point &p);
-
-void test1()
-{
-	Circle c(5, 0, 1);
-	Point p(3, 5);
-	std::cout << "点(3, 5)在圆内？" << isPointOnCircle(c, p) << std::endl;
-	p.setCoor(3, 4);
-	std::cout << "点(3, 4)在圆内？" << isPointOnCircle(c, p) << std::endl;
-	p.setCoor(4, 5);
-	std::cout << "点(4, 5)在圆内？" << isPointOnCircle(c, p) << std::endl;
-}
-
-Point fun()
-{
-	Point a(1, 2); 
-	std::cout << "上面是对象a" << std::endl;
-	return a; 
-}
-
-//拷贝构造函数的调用时机
-void test2()
-{
-	//Point p1(1, 2);
-	//Point p2(p1);
-	std::cout << "-------------------" << std::endl;
-	Point p3 = fun();
-}
-
-//类对象作为类成员
-void test3()
-{
-	Circle cir(5, 1, 1);
-}
-
-//静态成员变量
-void test4()
-{
-	//两种访问方式
-	
-	//1 通过对象访问
-	Point p1(1, 2);
-	std::cout << p1.value << std::endl;
-
-	Point p2(2, 3);
-	p2.value = 5;
-	std::cout << "通过另一个对象修改后：\n" << p1.value << std::endl;
-
-	//2 直接通过类名访问
-	std::cout << "\n\n" << Point::value << std::endl;
-
-}
-
-//静态成员函数
-void test5()
-{
-	Point p1, p2;
-	p1.fun();
-	p2.fun3();
-}
-
-int main()
-{
-	//test1();
-	//test2();
-	//test3();
-	//test4();
-	
-	int a = 10, b = 5;
-	std::cout << min(a, b) <<
-
-	//test5();
-	return 0;
-}
-
-
-int isPointOnCircle(Circle cir, Point &point)
-{
-	Point p = cir.getCenter();
-	double diff = abs(point.GetDistance(cir.getCenter()) - cir.getRadius());
-	return diff == 0 ? 0 : diff > 0 ? 1 : -1;
-}
+//#include <iostream>
+//
+//const double EPSILON = 1e-5;
+//
+//class Point
+//{
+//public:
+//	//无参构造函数
+//	Point() : X(0), Y(0) {}
+//	//有参构造函数
+//	Point(int x, int y) : X(x), Y(y) { std::cout << "Point构造函数调用" << std::endl; }
+//	Point(int x) :X(x), Y(0) {}
+//	//拷贝构造函数
+//	Point(Point& p) { X = p.getCoor_X(); Y = p.getCoor_Y(); std::cout << "Point拷贝构造调用" << std::endl; }
+//	Point(const Point& p) { X = p.X; Y = p.Y; std::cout << "Point(const)拷贝构造调用" << std::endl;
+//	}
+//	~Point() { std::cout << "Point析构函数调用" << std::endl; }
+//	//设置坐标
+//	void setCoor(int x, int y)
+//	{
+//		X = x;
+//		Y = y;
+//	}
+//	//获取位置
+//	int getCoor_X() { return X; }
+//	int getCoor_Y() { return Y; }
+//	//计算与另一个点之间的距离
+//	double GetDistance(Point p)
+//	{
+//		int xDis = p.getCoor_X() - getCoor_X();
+//		int yDis = p.getCoor_Y() - getCoor_Y();
+//		return sqrt(xDis * xDis + yDis * yDis);
+//	}
+//
+//	//静态成员变量
+//	static int value;
+//	static void fun()
+//	{
+//		int a = value;
+//		int b = m;
+//		std::cout << "静态成员函数" << std::endl;
+//	}
+//	void fun3() { fun2(); }
+//
+//private:
+//	int X;
+//	int Y;
+//	static int m;
+//	static void fun2();
+//};
+//
+//int Point::value = 0;
+//int Point::m = 10;
+//
+//void Point::fun2()
+//{
+//	std::cout << "静态成员函数" << std::endl;
+//}
+//
+//
+//class Circle {
+//public:
+//	Circle(int r, int x, int y) :Radius(r), Center(x, y) { std::cout << "Circle构造函数调用" << std::endl; }
+//	Circle(Circle& c) { Radius = c.getRadius(); Center = c.	getCenter(); std::cout << "Circle拷贝构造函数调用" << std::endl; }
+//	~Circle() { std::cout << "Circle析构函数调用" << std::endl; }
+//	void setCenter(int x, int y) { Center.setCoor(x, y); }
+//	void setRadius(int r) { Radius = r; }
+//	int getRadius() { return Radius; }
+//	Point getCenter() { return Center; }
+//private:
+//	//半径
+//	int Radius;
+//	//圆心坐标
+//	Point Center;
+//};
+//
+////判断点是否在圆上
+////返回值  大于0 在圆外；等于0 在圆上；小于0 在圆内
+//int isPointOnCircle(Circle c, Point &p);
+//
+//void test1()
+//{
+//	Circle c(5, 0, 1);
+//	Point p(3, 5);
+//	std::cout << "点(3, 5)在圆内？" << isPointOnCircle(c, p) << std::endl;
+//	p.setCoor(3, 4);
+//	std::cout << "点(3, 4)在圆内？" << isPointOnCircle(c, p) << std::endl;
+//	p.setCoor(4, 5);
+//	std::cout << "点(4, 5)在圆内？" << isPointOnCircle(c, p) << std::endl;
+//}
+//
+//Point fun()
+//{
+//	Point a(1, 2); 
+//	std::cout << "上面是对象a" << std::endl;
+//	return a; 
+//}
+//
+////拷贝构造函数的调用时机
+//void test2()
+//{
+//	//Point p1(1, 2);
+//	//Point p2(p1);
+//	std::cout << "-------------------" << std::endl;
+//	Point p3 = fun();
+//}
+//
+////类对象作为类成员
+//void test3()
+//{
+//	Circle cir(5, 1, 1);
+//}
+//
+////静态成员变量
+//void test4()
+//{
+//	//两种访问方式
+//	
+//	//1 通过对象访问
+//	Point p1(1, 2);
+//	std::cout << p1.value << std::endl;
+//
+//	Point p2(2, 3);
+//	p2.value = 5;
+//	std::cout << "通过另一个对象修改后：\n" << p1.value << std::endl;
+//
+//	//2 直接通过类名访问
+//	std::cout << "\n\n" << Point::value << std::endl;
+//
+//}
+//
+////静态成员函数
+//void test5()
+//{
+//	Point p1, p2;
+//	p1.fun();
+//	p2.fun3();
+//}
+//
+//class A {
+//public:
+//	A() : d(10) { m = 10; }
+//	~A() {}
+//	A(int m) : d(m) { this->m = m; }
+//
+//	void fun() 
+//	{ 
+//		std::cout << "调用func函数, " << std::endl;
+//		std::cout << "m = " << this->m << std::endl;
+//	}
+//
+//	int m;
+//	const int d;
+//	static int b;
+//	static const int n;
+//};
+//int A::b = 0;
+//const int A::n = 10;
+//
+//void test6()
+//{
+//	A a;
+//	A b(14);
+//	std::cout << sizeof(A) << std::endl;
+//	std::cout << sizeof(a) << std::endl;
+//	std::cout << b.m << std::endl;
+//}
+//
+//void test7()
+//{
+//	A* a = NULL;
+//	a->fun();
+//}
+//
+//int main()
+//{
+//	//test1();
+//	//test2();
+//	//test3();
+//	//test4();
+//	//test5();
+//	//test6();
+//	test7();
+//
+//	return 0;
+//}
+//
+//
+//int isPointOnCircle(Circle cir, Point &point)
+//{
+//	Point p = cir.getCenter();
+//	double diff = abs(point.GetDistance(cir.getCenter()) - cir.getRadius());
+//	return diff == 0 ? 0 : diff > 0 ? 1 : -1;
+//}
--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 
     string类型的赋值操作，通过assign来实现，或者直接赋值：
 
-```c
+```c++
 string s1("hello world"), s2;
 //s2 = s1;		这样也是可以的 
 s2.assign(s1);
@@ -65,14 +65,14 @@ C++中，两者的唯一区别：默认的访问权限不同
 
 - 括号法：
 
-    > ```c
+    > ```c++
     > Stu stu("张三", 18, "男");   //普通构造函数
     > Stu stu2(stu);				//拷贝构造函数
     > ```
 
 - 显式法：
 
-    > ```c
+    > ```c++
     > Person person = Person("张三", 18);	//普通构造函数
     > Person person1 = Person(person);	 //拷贝构造函数
     > ```
@@ -87,7 +87,7 @@ C++中，两者的唯一区别：默认的访问权限不同
     >
     > **当构造函数只有一个参数**，并且这个参数不是本类的const引用时，这种构造函数就称为转换构造函数
     >
-    > ```c
+    > ```c++
     > Person(string name) : Name(name) {}			//拷贝构造函数
     > ....
     > Person per = "张三";						   //转换构造函数
@@ -107,7 +107,7 @@ C++中，两者的唯一区别：默认的访问权限不同
 
 #### 拷贝构造函数的使用
 
-```c
+```c++
 Stu (const Stu& stu)
 {
 	Name = stu.getName();
@@ -129,7 +129,7 @@ Stu stu3 = stu1;
 
 - 用一个对象初始化另一个对象
 
-    > ```c
+    > ```c++
     > Person per1("张三", 18, "男");
     > Person per2(per1);
     > ```
@@ -138,7 +138,7 @@ Stu stu3 = stu1;
 
 - 用值传递形式给函数参数传值
 
-    > ```c
+    > ```c++
     > bool cmp(Line l1, Line l2)
     > {
     >     return l1.getLen() > l2.getLen();
@@ -154,7 +154,7 @@ Stu stu3 = stu1;
 
 - 以值的方式返回局部对象
 
-    > ```c
+    > ```c++
     > Point FindIntersection(Line l1, Line l2)//intersection: 交点
     > {
     >     Point intersection;
@@ -192,7 +192,7 @@ Stu stu3 = stu1;
 
 在栈区先进后出，后创建的对象先调用析构
 
-```c
+```c++
 void fun()
 {
     Person p1("张三");
@@ -205,7 +205,7 @@ void fun()
 
 ### 类对象作为类成员
 
-```c
+```c++
 class A {};
 class B
 {
@@ -235,7 +235,7 @@ A 和 B 的构造函数、析构函数的调用顺序是？
     - 在编译阶段分配内存
     - 类内声明，类外初始化
 
-    ```c
+    ```c++
     class A
     {
     public:
@@ -272,3 +272,284 @@ A 和 B 的构造函数、析构函数的调用顺序是？
 
 ## 对象模型和this指针
 
+### 成员变量和成员函数分开存储
+
+成员变量和成员函数是分开存储的，所有的对象共用一个函数实例。
+
+- 空类的大小：
+
+    ```c++
+    class A {};
+    ...
+    A a;
+    std::cout << sizeof(a) << std::endl;
+    ```
+
+    输出结果是1
+
+    本来一个空类型的实例，不包含任何东西，结果应该是0，但是**编译器为了区分空对象在内存的位置，会为每个对象分配一个字节的空间**
+
+    > 在声明空类型的实例的时候，必须占用一定的内存才能使用这些实例。**至少占用对少内存，由编译器决定，在Visual Studio中，每个空类型的实例占用1个字节。**
+
+- 对于非空类
+
+    - 非静态成员变量，属于类的对象
+    - 静态成员变量，不属于类的对象
+    - 成员函数，包括构造、析构，不属于类的对象
+
+    ```c++
+    class Person
+    {
+    	int m_A;				//非静态成员变量，属于类的对象上
+        static int m_B;			//静态成员变量，不属于类的对象上
+        void func()	{}			//非静态成员函数，不属于类的对象上
+        static void func2() {}	//静态成员函数，不属于类的对象上
+    };
+    int Person::m_B;
+    ```
+
+    即：**只有非静态成员变量属于类的对象**，其他的都不属于，在存储上其他的都不算做对象大小
+
+### this指针
+
+#### 概念
+
+每一个非静态成员函数都只会诞生一份函数实例，那个多个同类型对象就会共用一块代码，这块代码是怎么区分那个对象调用自己的？——this指针
+
+**this指针指向被调用的成员函数所属的对象**
+
+- this指针是隐含每一个**非静态成员函数内**的一种指针
+- this指针不需要定义，直接使用即可
+
+
+
+#### this指针的用途：
+
+- 当形参与成员变量同名时，可以用this指针来区分
+
+    > 为了避免这种冲突，可以在成员变量前加上前缀 m_    m是member的意思
+    >
+    > 例如 int m_Age;   这样就可以跟普通形参  age 区分开了
+
+- 在类的非静态成员函数中返回对象本身，可使用return *this
+
+    ```c++
+    class Person{
+        ...
+        Person& AddNum();
+    };
+    
+    Person& Person::AddNum(Person p)
+    {
+        this->num += p.num;
+        return *this;
+    }
+    ...
+        
+    Person p(10);
+    Person p2(0);
+    //链式编程思想
+    p2.AddNum(p).AddNum(p).AddNum(p);
+    ```
+
+    这里函数类型是Person& 返回对象本身，如果是Person，则是另一个对象了，就不能链式的使用了。
+
+#### 注意事项
+
+空指针也可以调用成员函数，但是这是this指针为空，这个时候访问属性就会出错
+
+```c++
+class Person{
+    void func() { std::cout << "hello world" << std::endl; }
+  	void func2() { std::cout << name << std::endl; }
+  	string name;  
+};
+
+Person *p = NULL;
+p.func();
+p.func2();					//错误，this指向空，原理相当于下面这句
+cout << p->name << endl;	//当然是错的
+```
+
+在成员函数中访问属性name ， cout << name; 相当于 cout << this->name;  而空指针对象的this也是指向NULL的，这样this->name就出错了
+
+> 加上一个判断语句，加强代码健壮性：
+>
+> ```c++
+> if(this == NULL)
+>     return;
+> ```
+>
+> 
+
+## const修饰成员
+
+#### 饰成员函数 : 常函数
+
+- 成员函数后加上const后称这个函数为**常函数**
+- 常函数内不可以修改成员属性
+- 成员属性声明时加上关键字**mutable**后，在常函数中依然可以修改
+
+this指针的本质：
+
+```c++
+class Person{
+    ...
+    void func();
+};
+
+void Person::func()
+{
+    cout << this->name << endl;
+}
+```
+
+在这个类中，this的类型为:Person* const this;  这代表了this的指向不可以改变，但是this指向的值可以改变。那么要想this指向的值也不能改变，就应该定义为const Person* const this;
+
+常函数的原理就是这个：类型  函数名()  const  {}
+
+在大括号之前加上const，就代表这个成员函数中this指向的值不可以修改，即**这个成员函数中不能修改属性**，如果一定要修改，就要在属性定义前加上**mutable**关键字
+
+```c++
+class Person{
+	void func(int c) const { a = c; }
+    void func2(int c) const { b = c; }	//出错，常函数中不能改变普通属性
+    
+    int b;
+    mutable int a;
+};
+```
+
+#### 修饰成员属性
+
+在属性前面加上const就成了常数据成员，**在任何函数中都不能对该成员赋值**，因此需要对其初始化。在构造函数中，只能通过初始化列表获取初始值，进行初始化。
+
+```c++
+class Person{
+    Person(int m) : a(m) {}
+    
+    const int a;
+    static const int b;
+    //或者是
+    //static const int b = 10;
+};
+const int Person::b = 10;
+```
+
+#### 修饰对象：常对象
+
+- 声明对象前加上const称为常对象
+- 常对象只能调用常函数
+
+## 友元
+
+友元关系提供了不同类或对象的成员函数之间、类的成员函数与一般函数之间进行数据共享的机制。
+
+简单来说就是通过友元关系，可以在普通函数与类之间建立特殊关系，使得普通函数可以直接访问类的私有属性，同样，这个类与另一个类之间也可以建立友元关系，使得可以访问私有属性：
+
+#### 全局函数做友元
+
+```c++
+class A{
+    friend void setVal(A *a, int m);
+public:
+    A(int a) : m_a(a) {}
+private:
+    int m_a;
+};
+
+void setVal(A *a, int m)
+{
+    a->m_a = m;
+}
+```
+
+通过加上friend关键字，就可以声明一个友元关系，来让函数setVal(...)访问类A的私有属性
+
+
+
+#### 类做友元
+
+```c++
+class A{
+public:
+    void display() { cout << X << endl; }
+    int getX() { return X; }
+private:
+    int X;
+};
+
+class B{
+public:
+    void setX(int i);
+private:
+    A a;
+}
+void B::setX(int i)
+{
+    a.X = i;
+}
+```
+
+这里setX肯定是不行的，因为函数中访问了A的私有属性 X ，而要实现这种功能，需要在A中声明类B为A的一个友元类，在A中添加
+
+```c++
+friend class B;
+```
+
+声明之后，在B类中所有的成员函数都可以访问A的私有属性了 
+
+#### 成员函数做友元
+
+如果只是想要某一个成员函数作为友元函数，就不可以将整个类作为另一类的友元，只需要在函数前加上friend关键字，并且加上这个成员函数所属的类名作为限定就可以了
+
+**但是会出现一些问题**
+
+```c++
+class A;
+class B{
+public:
+    void func() { a.x = 10; }
+    A a;
+};
+
+class A{
+    friend void B::func();	//声明类B的成员函数func()为类A的一个友元
+private:
+    int x;
+}
+```
+
+看起来这么写是没有问题的，然而编译后报错：line 4, 使用了未定义的类型"A"
+
+原因是B中的func()函数中使用了对象 a ，然而此时类A只有一个前行引用声明，对于A的实际大小并不确定，所以就会出错，解决方案是将函数func()的实现放在类A的实现后面
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+ 
