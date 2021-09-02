8f5de609a46d4f7f7ac0f7e1d479d98d489c629f
zzz1114 <2759579990@qq.com>
2021-09-02 22:43:02 +0800

    临时对象只能绑定到常引用、函数调用符号的重载（伪函数）

--- OOP/赋值运算符重载.cpp
+++ OOP/赋值运算符重载.cpp
@@ -2,14 +2,12 @@
 using namespace std;
 
 class Complex {
-	friend std::ostream& operator<<(std::ostream& cout, Complex& c);
+	friend std::ostream& operator<<(std::ostream& cout,const Complex& c);
 public:
 	Complex(int i, int j) : m_Real(i), m_Imag(j) { }
 	Complex operator+(Complex& c2);
-	Complex operator-(Complex& c2);
 	Complex& operator=(Complex& c2);
-
-	void Show() const { cout << m_Real << " + " << m_Imag << "i" << endl; }
+	void operator()(string str) { cout << str << endl; }
 private:
 	//Êµ²¿
 	int m_Real;
@@ -19,28 +17,17 @@ private:
 
 Complex Complex::operator+(Complex& c2)
 {
-	Complex temp(0, 0);
-	temp.m_Imag += c2.m_Imag;
-	temp.m_Real += c2.m_Real;
-	return temp;
-}
-
-Complex Complex::operator-(Complex& c2)
-{
-	this->m_Imag -= c2.m_Imag;
-	this->m_Real -= c2.m_Real;
-	return *this;
+	return Complex(m_Real + c2.m_Real, m_Imag + c2.m_Imag);
 }
 
 Complex& Complex::operator=(Complex& c2)
 {
-	Complex temp(0, 0);
-	temp.m_Imag = c2.m_Imag;
-	temp.m_Real = c2.m_Real;
-	return temp;
+	m_Real = c2.m_Real;
+	m_Imag = c2.m_Imag;
+	return *this;
 }
 
-std::ostream& operator<<(std::ostream& cout, Complex& c)
+std::ostream& operator<<(std::ostream& cout, const Complex& c)
 {
 	cout << c.m_Real << ((c.m_Imag > 0) ? " + " : " - ") << abs(c.m_Imag) << "i ";
 	return cout;
@@ -50,19 +37,10 @@ void test01()
 {
 	Complex c1(1, 2);
 	Complex c2(2, 5);
-	Complex c3(-2, -3);
-	cout << "c1: " << c1 << endl;
-	cout << "c2: " << c2 << endl;
-	cout << "c3: " << c3 << endl;
-	cout << "c1 + c2 = " << c1 + c2 << endl;
-	cout << "c1: " << c1 << endl;
-	cout << "c2: " << c2 << endl;
-	cout << "c3: " << c3 << endl;
-	c3 = c1 + c2;
-	cout << "c1: " << c1 << endl;
-	cout << "c2: " << c2 << endl;
-	cout << "c3: " << c3 << endl;
-	//a.operator+(b).Show();
+	const Complex c3(1, 4);
+	cout << c1 << c2;
+	c1("hello world ");
+	//cout << c1 + c2;
 }
 
 int main()
